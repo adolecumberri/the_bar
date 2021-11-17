9c2e9e0da0a6cb18a9ac1537a21c1bdff1156dda
adolecumberri <adolecumberri@gmail.com>
2021-11-17 22:52:07 +0100

    correcciones menor del tooltip

--- the_bar/src/componentes/ToolTipGlobal.tsx
+++ the_bar/src/componentes/ToolTipGlobal.tsx
@@ -16,10 +16,6 @@ interface IToolTip {
 
 const ToolTipGlobal: FC<IToolTip> = ({ children, key, content, hidden }) => {
 
-    // const {top, left} = variation as  {
-    //     top: number;
-    //     left: number;
-    // } || { top : 0, left: 0 }
     const { pixelSize } = useContext(StyleContext);
 
     if (hidden) {
@@ -28,7 +24,6 @@ const ToolTipGlobal: FC<IToolTip> = ({ children, key, content, hidden }) => {
 
     return (
         <>
-
             <div
                 key={key ? key : uniqueID()}
                 style={{
@@ -42,29 +37,13 @@ const ToolTipGlobal: FC<IToolTip> = ({ children, key, content, hidden }) => {
                     border: "1px solid black",
                     padding: "16px",
                     zIndex: 2,
-                    minWidth: `${TILE_SIZE * pixelSize * 3}px`,
-                     maxWidth: `${TILE_SIZE * pixelSize * 6}px`,
+                    minWidth: `${(TILE_SIZE * pixelSize) + 32}px`,
+                     maxWidth: `${(288) + 32}px`,
                     minHeight: `${TILE_SIZE * pixelSize * 4}px`
                 }}
             >
-                {/* tittle */}
                 {children}
             </div>
-            {/* <div style={{
-                width: `${6 * pixelSize}px`,
-                height: `${6 * pixelSize}px`,
-                background: "white",
-                position: "absolute",
-                top: ` ${((TILE_SIZE * pixelSize) - (3 * pixelSize)) + 11 + top}px`,
-                left: `${3 * pixelSize + left}px`,
-                zIndex: 2,
-                transform: "rotate(45deg)",
-                borderTopLeftRadius: `${ 2 + pixelSize }px`
-                // borderTop: "1px solid black",
-                // borderLeft: "1px solid black"
-
-            }}
-            ></div> */}
         </>
     );
 };
